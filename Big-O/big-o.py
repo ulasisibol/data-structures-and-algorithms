dd5f31c55f3721c2b2549751e653af2131510bd0
Ulaş İşibol <ulasisbl@gmail.com>
2024-06-16 23:42:56 +0300

    big o space complexity examples

--- Big-O/big-o.py
+++ Big-O/big-o.py
@@ -35,3 +35,53 @@ def logn(n):
         print(n)
 
 logn(8)
+
+
+import math
+def nlogn(n):
+    lim= n
+    while n > 1:
+        n= math.floor(n/2)
+        for i in range(1,lim):
+            print(i)
+
+nlogn(16)
+
+
+
+def twotonfibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return twotonfibonacci(n-1) + twotonfibonacci(n-2)
+
+twotonfibonacci(9)
+
+
+
+def nfactorial(n):
+    if n == 0:
+        print("1")
+        return
+    else:
+        for i in range(0,n):
+            print(i)
+            nfactorial(n-1)
+
+nfactorial(3)
+
+
+counter = 0
+def actualFactorial(n):
+    global counter
+    counter += 1
+    print(counter)
+    if (n==1 or n==0):
+        return 1
+    else:
+        return (n * actualFactorial(n - 1))
+    
+actualFactorial(8)
+
+
